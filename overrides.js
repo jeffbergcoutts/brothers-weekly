3f101b370651da328aed978d5fe9092e825de052
Jeff Bergcoutts <bergcoutts@spotify.com>
2020-07-10 09:20:06 +0200

    add to denylist and create git ignore file

--- overrides.js
+++ overrides.js
@@ -2,7 +2,12 @@ const overrides = {
     denylist: {
         tracks: [],
         albums: [],
-        artists: ["1V95p8oWGxXlpcFRYqXV3w", "1XzY7sGHYarxdc9u2Ks1oU"]
+        artists: [
+            "1V95p8oWGxXlpcFRYqXV3w",
+            "1XzY7sGHYarxdc9u2Ks1oU",
+            "6YYk2yfx7jUHLfI5W1sgNi",
+            "7oWSqrgMuIEyH9qp5nu2e5"
+        ]
     }
 }
 
