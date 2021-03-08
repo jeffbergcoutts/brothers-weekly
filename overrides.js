849df381a133689c1eeb659702df23de2033690b
Jeff Bergcoutts <bergcoutts@spotify.com>
2021-03-08 11:08:22 +0100

    update deny list and clarify modes

--- overrides.js
+++ overrides.js
@@ -1,13 +1,17 @@
 const overrides = {
     denylist: {
         tracks: [],
-        albums: [],
+        albums: ["6AVhZFoVQNjVKTb2lVNokw"],
         artists: [
             "1V95p8oWGxXlpcFRYqXV3w",
             "1XzY7sGHYarxdc9u2Ks1oU",
             "6YYk2yfx7jUHLfI5W1sgNi",
             "7oWSqrgMuIEyH9qp5nu2e5",
-            "0w5SU4gzff0dsC2aohgIzU"
+            "0w5SU4gzff0dsC2aohgIzU",
+            "6nCO4Jv7mU99OS6jLevYVH",
+            "2I9JVgirByTBg7g4EHNQh5",
+            "6VF93ZcQqYC1kLJdcsmuex",
+            "2A3DK9TwLnlkjI5OMITTjQ"
         ]
     }
 }
