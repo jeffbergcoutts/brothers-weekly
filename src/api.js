8929e4b239189ce332b8bdeddc79c7949ca3ecbe
Jeff Bergcoutts <bergcoutts@spotify.com>
2021-03-12 20:00:34 +0100

    samesite fix

--- src/api.js
+++ src/api.js
@@ -19,7 +19,7 @@ var requestSessionHandler = sessions({
   duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
   activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
   cookie: {
-    SameSite: None
+    sameSite: 'none'
   }
 })
 
