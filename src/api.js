d0da740409423e48cbe7de0118d8c9f6912167a8
Jeff Bergcoutts <bergcoutts@spotify.com>
2021-03-12 16:03:20 +0100

    revert setting domain

--- src/api.js
+++ src/api.js
@@ -17,10 +17,7 @@ var requestSessionHandler = sessions({
   cookieName: 'authTokens', // cookie name dictates the key name added to the request object
   secret: cookieSecret, // should be a large unguessable string
   duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
-  activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
-  cookie: {
-    domain: 'api.sharedweekly.com'
-  }
+  activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
 })
 
 function filterResults(results, filter) { //RE-WRITE & move?
