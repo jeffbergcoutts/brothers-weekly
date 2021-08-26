3a37fb84c940c7aebe60816572918ef256f56dc8
Jeff Bergcoutts <bergcoutts@spotify.com>
2021-08-26 13:04:27 +0200

    cookie test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const path = require('path');
-const express = require("express");
+const express = require('express');
+const session = require('express-session');
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+app.set('trust proxy', 1); // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+app.use((req,res, next) => {
+  req.session.test = "cookie value";
+  next();
+});
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -9,6 +25,10 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get("/cookie", (req, res) => {
+  res.send(req.session.test);
 });
 
 // All other GET requests not handled before will return our React app
