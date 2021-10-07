0b1ae4de58a16ae6bdde9aa557fb58ff5ff67620
Jeff Bergcoutts <bergcoutts@spotify.com>
2021-10-07 14:12:37 +0200

    simplify and re-factor run file for ease of use

--- run.js
+++ run.js
@@ -1,37 +1,23 @@
 const playlistGenerator = require("./playlistGenerator");
 
-const date = new Date();
+const option1 = process.argv[2] || "" // run mode, 'publish' will modify real playlists, 'test' will modify test playlists, 'report' will only simulate run
+const option2 = process.argv[3] || "" // manual override to run on days other than Monday, use 'manual' to run
+const option3 = process.argv[4] || "" // month override to test playlist order, enter number, starting with 0 for Jan to 11 for Dec
 
-const day = date.getDay();
-console.log(day)
+// Check Run Mode
+const mode = (!option1) ? "" : option1
+console.log(`mode: ${mode}`)
 
-const option1 = process.argv[2] || "" // run mode
-const option2 = process.argv[3] || "" // manual override to run on days other than Monday
-const option3 = process.argv[4] || "" // month override to test playlist order
-
-// get run mode from user input
-switch(option1) {
-  case "publish": // will modify archive & real playlist
-    mode = "publish"
-    break;
-  case "test": // will modify test archive and test playlist
-    mode = "test"
-    break;
-  case "report": // will not modify playlists
-    mode = "report"
-    break;
-  default:
-    mode = ""
-    break;
-}
-
-console.log(mode)
 if (mode === "") {
   console.log("You must select a mode. 'publish' will update real playlists, 'test' will update test playlists, 'report' will only print results")
   return;
 }
 
- // determine whether to use the real month or a user input to determine playlist order
+// Set Run Options
+const date = new Date();
+const day = date.getDay();
+
+ // use the real month or a user input to determine playlist member order
 let month = ""
 if (option3 != "") {
   month = parseInt(option3);
@@ -41,7 +27,7 @@ if (option3 != "") {
   console.log(`the current month is ${month}`)
 }
 
-// only run on Mondays, of if useroveride entered
+// only run on Mondays, of if overide entered
 if ((day === 1) || (option2 === "manual")) {
   playlistGenerator.getAllTracksAndCreatePlaylist(mode, month)
 } else {
