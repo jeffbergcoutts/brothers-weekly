38924fa5b9ece5b4c8cf9e63cdb74f347fdec084
Jeff Bergcoutts <bergcoutts@spotify.com>
2021-10-18 11:17:08 +0200

    example JSON and clean up dataTransformer

--- server/dataTransformer.js
+++ server/dataTransformer.js
@@ -1,14 +1,3 @@
-function Track(albumId, artistId, trackId, albumUrl) {
-  this.albumId = albumId;
-  this.artistId = artistId;
-  this.trackId = trackId;
-  this.albumUrl = albumUrl;
-}
-
-function Response(tracks) {
-  return tracks
-}
-
 function transformTracksResponse(playlist) {
   // takes a JSON response from the Spotify Web API with all tracks from a Playlist
   // returns a JSON object with just the data needed to display in the front end
@@ -28,7 +17,6 @@ function transformTracksResponse(playlist) {
     })
   ))
 
-  // const data = JSON.stringify(testData)
   return JSON.stringify(data)
 }
 
